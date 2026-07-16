7579aa6194a0dc0e5d447b53be48ca0c51ee729e
inkdmontana <tvazquez92@gmail.com>
2026-07-16 14:35:20 -0400

    fix amplify frontend

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ CORS(
     origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://main.dsotr4fe9n042.amplifyapp.com/"
+        "https://main.dsotr4fe9n042.amplifyapp.com"
     ]
 )
 
