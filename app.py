05258d9222281134576006153bc74b8cc28d2812
inkdmontana <tvazquez92@gmail.com>
2026-07-16 11:08:37 -0400

    add ampplify frontend to cors

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ CORS(
     app,
     origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://main.d13gf6lyjwei0.amplifyapp.com"
     ]
 )
 
